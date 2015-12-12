8dd7a2cf0bc4b254d498e179b8e8c0c3ab275981
softengilker <ilkerkonar@gmail.com>
2015-12-12 13:14:42 +0200

    Initial project commit

---
+++ Progfun_Martin_Odersky_Coursera/src/main/scala/com/ilkerkonar/progfun/assignments/week1/pascalstriangle/PascalTriangleMain.scala
@@ -0,0 +1,21 @@
+package com.ilkerkonar.progfun.assignments.week1.pascalstriangle
+
+object PascalTriangleMain {
+
+	def main( args : Array[ String ] ) {
+
+		println( "Pascal's Triangle" )
+
+		for ( row <- 0 to 10 ) {
+
+			for ( col <- 0 to row ) {
+
+				print( pascal( col, row ) + " " )
+			}
+
+			println()
+		}
+	}
+
+	def pascal( c : Int, r : Int ) : Int = if ( c == 0 || c == r ) 1 else pascal( c, r - 1 ) + pascal( c - 1, r - 1 )
+}
---
+++ Progfun_Martin_Odersky_Coursera/src/test/scala/com/ilkerkonar/progfun/assignments/week1/pascalstriangle/PascalTriangleMainSuite.scala
@@ -0,0 +1,30 @@
+package com.ilkerkonar.progfun.assignments.week1.pascalstriangle
+
+import org.scalatest.FunSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import PascalTriangleMain.pascal
+
+@RunWith( classOf[ JUnitRunner ] )
+class PascalTriangleMainSuite extends FunSuite {
+
+	test( "pascal: col=0,row=2" ) {
+		assert( pascal( 0, 2 ) === 1 )
+	}
+
+	test( "pascal: col=1,row=2" ) {
+		assert( pascal( 1, 2 ) === 2 )
+	}
+
+	test( "pascal: col=1,row=3" ) {
+		assert( pascal( 1, 3 ) === 3 )
+	}
+
+	test( "pascal: col=2,row=4" ) {
+		assert( pascal( 2, 4 ) === 6 )
+	}
+
+	test( "pascal: col=3,row=6" ) {
+		assert( pascal( 3, 6 ) === 20 )
+	}
+}
